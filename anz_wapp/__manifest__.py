6ca6e4082767c15ea5a93e4c4e6eaa7afee10871
maxisoko <93795535+maxisoko@users.noreply.github.com>
2022-05-04 23:40:45 -0300

    [add]anz_wapp
    Se agrega funcionalidad de link de Whatsapp para los campos de tipo telefono.

---
+++ anz_wapp/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Link WhatsApp - Ansuz Solutions",
+    "summary": "Link WhatsApp",
+    "author": "Ansuz Solutions",
+    "website": "https://www.ansuzsolutions.com/",
+    "category": "",
+    "version": "1.0.0",
+    "assets": {
+        'web.assets_backend': {
+            '/anz_wapp/static/src/js/fields_wapp_widget.js',
+        }
+    },
+    'depends': [
+        'base', 'web'
+    ],
+    'data': [
+
+    ]
+}
+
