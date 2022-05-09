7af46a9665bb2ca534381a2e039c722b11dbb5d6
maxisoko <93795535+maxisoko@users.noreply.github.com>
2022-05-09 12:17:17 -0300

    add anz_crm_wapp

---
+++ anz_crm_wapp/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "CRM Link WhatsApp - Ansuz Solutions",
+    "summary": "CRM Link WhatsApp",
+    "author": "Ansuz Solutions",
+    "website": "https://www.ansuzsolutions.com/",
+    "category": "",
+    "version": "1.0.0",
+    "license": 'LGPL-3',
+    "assets": {
+        'web.assets_backend': {
+            '/anz_crm_wapp/static/src/css/fields_crm_wapp_widget.css',
+        }
+    },
+    'depends': [
+        'anz_wapp', 'crm'
+    ],
+    'data': [
+        'views/crm_lead_views_inherit.xml',
+    ]
+}
+
